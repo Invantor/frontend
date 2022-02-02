3e3e78c1c1c9831ae61712b6492b0b060c6ad71d
Si Yang Cui <siyangcui@gmail.com>
2022-02-02 18:30:04 +1100

    fixed up error with uncontrolled component for toggle of user activity

--- src/components/userActiveToggle.jsx
+++ src/components/userActiveToggle.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import * as React from "react";
 import Switch from "@mui/material/Switch";
 
 import api from "../api/api";
-import { Co2Sharp } from "@mui/icons-material";
+import GlobalContext from "../context/globalContext";
 
 const UserActiveToggle = (props) => {
+  const { global } = useContext(GlobalContext);
   const { user } = props;
   const [isActive, setIsActive] = useState(user.is_active);
-
-  console.log(user);
 
   const switchOnChangeHandler = async () => {
     console.log("in toggle handler");
     console.log(!isActive);
     await api.editUserStatus(user.id, !isActive);
-    setIsActive(!isActive);
+    // setIsActive(!isActive);
   };
 
   return (
