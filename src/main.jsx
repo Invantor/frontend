b592450bc79685a7f811f9f34821ae49dc2a5121
Si Yang Cui <siyangcui@gmail.com>
2022-02-09 10:07:57 +1100

    imported style.css to main.jsx to try and fix netlify deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
+import "./style.css";
 import invantorTheme from "./components/utilities/theme";
 
 import "@fontsource/roboto/300.css";
