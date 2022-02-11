32379dc8f24496aa6178a67b1d8d0d134e9a2c2b
LukeHooper <laaronhooper@gmail.com>
2022-02-11 18:07:35 +1100

    test cases written for mixers and drinks

--- cypress/integration/drinks.spec.js
+++ cypress/integration/drinks.spec.js
@@ -4,7 +4,7 @@ describe("invantor application", () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
+    // Since we want to is it the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
     // cy.visit(`${Cypress.config("baseUrl")}`);
     cy.clearLocalStorage();
@@ -15,198 +15,164 @@ describe("invantor application", () => {
     cy.findByRole("tab", { name: "Drinks" }).click();
   });
 
-  it("Drink should NOT be created with Missing Volume", () => {
+  it("Drink should NOT be created with Missing Name", () => {
     cy.findByRole("button", { name: "Create" }).click();
-    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-    cy.findByRole("button", { name: "alcohol_id" }).click();
-    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-    cy.findByRole("textbox", { name: "Critical Volume" }).type("500");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
     cy.findByRole("button", { name: "Submit" }).click();
     cy.findByRole("alert")
-      .should("contain", "Volume in ml can't be blank")
+      .should("contain", "Name can't be blank")
       .and("be.visible");
   });
 
-  // it("Drink should NOT be created with Missing Name", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("500");
-  //   cy.findByRole("textbox", { name: "Critical Volume" }).type("500");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Name can't be blank")
-  //     .and("be.visible");
-  // });
+  it("Drink should NOT be created with Missing Alcohol", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Alcohol must exist")
+      .and("be.visible");
+  });
 
-  // it("Drink should NOT be created with missing Critical Volume", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("500");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Critical volume can't be blank")
-  //     .and("be.visible");
-  // });
+  it("Drink should NOT be created with Missing Alcohol Amount", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Alcohol amount can't be blank")
+      .and("be.visible");
+  });
 
-  // it("Drink should NOT be created with When Volume in mL is not an integer", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("Five Hundred");
-  //   cy.findByRole("textbox", { name: "Critical Volume" }).type("500");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Volume in ml is not a number")
-  //     .and("be.visible");
-  // });
 
-  // it("Drink should NOT be created with When Critical Volume not an integer", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("500");
-  //   cy.findByRole("textbox", { name: "Critical Volume" }).type("Five Hundred");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Critical volume is not a number")
-  //     .and("be.visible");
-  // });
+  it("Drink should NOT be created with Missing Mixer", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Mixer must exist")
+      .and("be.visible");
+  });
 
-  // it("Drink SHOULD be created When correct parameters are passed.", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("500");
-  //   cy.findByRole("textbox", { name: "Critical Volume" }).type("1000");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Successfully Created")
-  //     .and("be.visible");
-  // });
-  // it("Drink should NOT be created When an mixer with the same name already exists.", () => {
-  //   cy.findByRole("button", { name: "Create" }).click();
-  //   cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
-  //   cy.findByRole("textbox", { name: "Volume In Ml" }).type("500");
-  //   cy.findByRole("textbox", { name: "Critical Volume" }).type("1000");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Name has already been taken")
-  //     .and("be.visible");
-  // });
+  it("Drink should NOT be created with Missing Mixer", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Mixer amount can't be blank")
+      .and("be.visible");
+  });
 
-  // it("Search function SHOULD return the matching entry", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .should("have.length", 1)
-  //     .and("contain", "New_Drink_Entry");
-  // });
+  it("Drink SHOULD be created", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Successfully Created")
+      .and("be.visible");
+  });
+  
+  it("Second drink SHOULD be created", () => {
+    cy.findByRole("button", { name: "Create" }).click();
+    cy.findByRole("textbox", { name: "Name" }).type("New_Drink_Entry_2");
+    cy.get("#alcoholid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Alcohol Amount" }).type("100");
+    cy.get("#mixerid").click();
+    cy.findByRole("listbox", { tabindex: -1 }).click();
+    cy.findByRole("textbox", { name: "Mixer Amount" }).type("100");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Successfully Created")
+      .and("be.visible");
+  });
+  
+it("Edit button SHOULD bring up the edit modal when clicked", () => {
+    cy.findByRole("button", { name: "Show" }).click();
+    cy.get('[aria-label="simple table"]')
+      .contains('tr', 'New_Drink_Entry')
+      .find("[data-cy=edit-button]")
+      .click();
+  // cy.get("#MuiBox-root css-1acyv19")
+    // .should('be.visable')
+});
+  
+it("Edit should NOT be successful, duplicate error", () => {
+  cy.findByRole("button", { name: "Show" }).click();
+    cy.get('[aria-label="simple table"]')
+      .contains('tr', 'New_Drink_Entry')
+      .find("[data-cy=edit-button]")
+      .click();
+   cy.get("[data-cy=edit-name]").clear().type("New_Drink_Entry_2");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Name has already been taken")
+      .and("be.visible");
+});
+  
+it("Edit should successfully edit alcohol amount", () => {
+  cy.findByRole("button", { name: "Show" }).click();
+    cy.get('[aria-label="simple table"]')
+      .contains('tr', 'New_Drink_Entry')
+      .find("[data-cy=edit-button]")
+      .click();
+   cy.get("[data-cy=edit-alcohol-amount]").clear().type("101");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Successfully Updated")
+      .and("be.visible");
+});
+  
+it("Edit SHOULD successfully edit mixer amount", () => {
+  cy.findByRole("button", { name: "Show" }).click();
+    cy.get('[aria-label="simple table"]')
+      .contains('tr', 'New_Drink_Entry')
+      .find("[data-cy=edit-button]")
+      .click();
+   cy.get("[data-cy=edit-mixer-amount]").clear().type("101");
+    cy.findByRole("button", { name: "Submit" }).click();
+    cy.findByRole("alert")
+      .should("contain", "Successfully Updated")
+      .and("be.visible");
 
-  // it("Edit button SHOULD bring up the edit modal when clicked", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
+  // it("Delete SHOULD successfully edit mixer amount", () => {
+  // cy.findByRole("button", { name: "Show" }).click();
+  //   cy.get('[aria-label="simple table"]')
+  //     .contains('tr', 'New_Drink_Entry')
   //     .find("[data-cy=edit-button]")
   //     .click();
-  //   cy.get("[data-cy=edit-modal]").should("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited to a name that already exists in the system", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-name]").clear().type("Jim Beam");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Name has already been taken")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited if Name is blank", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-name]").clear();
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Name can't be blank")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited if Volume in ML is blank", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-volume]").clear();
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Volume in ml can't be blank")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited Critical Volume is blank", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-criticalvolume]").clear();
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Critical volume can't be blank")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited if volume is not an interger", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-volume]").clear().type("Five Hundred");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Volume in ml is not a number")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink should NOT be successfully edited if critical volume is not an interger", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-criticalvolume]").clear().type("Five Hundred");
-  //   cy.findByRole("button", { name: "Submit" }).click();
-  //   cy.findByRole("alert")
-  //     .should("contain", "Critical volume is not a number")
-  //     .and("be.visible");
-  // });
-
-  // it("Drink SHOULD be successfully edited if correct parameters are provided", () => {
-  //   cy.findByRole("button", { name: "Show" }).click();
-  //   cy.findByRole("textbox", { name: "Search..." }).type("New_Drink_entry");
-  //   cy.get("[data-cy=table-body]")
-  //     .find("[data-cy=table-row]")
-  //     .find("[data-cy=edit-button]")
-  //     .click();
-  //   cy.get("[data-cy=edit-name]").clear().type("New_Drink_Entry_V2");
-  //   cy.get("[data-cy=edit-volume]").clear().type("1000");
-  //   cy.get("[data-cy=edit-criticalvolume]").clear().type("2000");
+  //  cy.get("[data-cy=edit-mixer-amount]").clear().type("101");
   //   cy.findByRole("button", { name: "Submit" }).click();
   //   cy.findByRole("alert")
   //     .should("contain", "Successfully Updated")
   //     .and("be.visible");
-  // });
+});
 });
 ``;
