30724bbc57053acb904fc126fba5df5374a469b1
Si Yang Cui <siyangcui@gmail.com>
2022-02-13 13:07:58 +1100

    styled low stock component and added export to csv function

--- src/components/home/lowStock.jsx
+++ src/components/home/lowStock.jsx
@@ -8,22 +8,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Grid from "@mui/material/Grid";
-
-const boxStyle = {
-  border: 5,
-  color: "secondary.main",
-  height: "300px",
-  width: 0.8,
-  display: "flex",
-  justifyContent: "center",
-  flexDirection: "row",
-  flexWrap: "wrap",
-  borderRadius: "16px",
-  minHeight: "300px",
-  overflow: "hidden",
-  overflowY: "scroll",
-};
+import { CSVLink } from "react-csv";
 
 const LowStock = (props) => {
   const { alcohols, mixers } = props;
@@ -56,41 +41,63 @@ const LowStock = (props) => {
     setState(combined);
   }, [alcohols, mixers]);
 
+  const headers = [
+    { label: "Name", key: "name" },
+    { label: "Volume", key: "volume_in_ml" },
+    { label: "Critical Volume", key: "critical_volume" },
+  ];
+
+  const csvExport = {
+    filename: "export.csv",
+    headers: headers,
+    data: state,
+  };
+
   return (
-    <Grid container justifyContent="center">
-      <Box sx={boxStyle}>
-        <Typography>Low Stock</Typography>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell align="center">Name</TableCell>
-                <TableCell align="center">Volume</TableCell>
-                <TableCell align="center">Critical Volume</TableCell>
+    <Box>
+      <Typography align="center" variant="h5" color="primary">
+        Low Stock Items
+      </Typography>
+      <TableContainer
+        component={Paper}
+        sx={{
+          height: 400,
+        }}
+      >
+        <Table
+          sx={{ minWidth: 650, bgcolor: "primary", height: "max-content" }}
+          aria-label="simple table"
+          stickyHeader
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Volume</TableCell>
+              <TableCell align="center">Critical Volume</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {state.map((item, i) => (
+              <TableRow
+                key={i}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  {item.name}
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  {item.volume_in_ml}
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  {item.critical_volume}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {state.map((item, i) => (
-                <TableRow
-                  key={i}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="center" component="th" scope="row">
-                    {item.name}
-                  </TableCell>
-                  <TableCell align="center" component="th" scope="row">
-                    {item.volume_in_ml}
-                  </TableCell>
-                  <TableCell align="center" component="th" scope="row">
-                    {item.critical_volume}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </Grid>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <CSVLink {...csvExport}>Export to CSV</CSVLink>
+    </Box>
   );
 };
 
